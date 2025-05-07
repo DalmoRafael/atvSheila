0067216f9f759afac2ab03eae51674a29aa9c565
dalmorafael <104334002+DalmoRafael@users.noreply.github.com>
2025-05-07 08:37:15 -0300

    Update Main.java

--- Questao1/Main.java
+++ Questao1/Main.java
@@ -7,9 +7,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         List<Tarefa> tarefas = new ArrayList<>();
-
         int opc, index;
-
         do {
             System.out.println("1. Adicionar tarefa");
             System.out.println("2. Mostrar tarefas");
